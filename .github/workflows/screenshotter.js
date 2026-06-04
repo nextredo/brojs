011d9838f2fbeae9c78259ff73fa96db0fd293bd
nextredo <kb7737@gmail.com>
2026-06-04 15:58:26 +1000

    fix(cicd): fix cicd pt.10 (please)

--- .github/workflows/screenshotter.js
+++ .github/workflows/screenshotter.js
@@ -1,27 +1,29 @@
-// NOTE:
-// The first custom argument should be the path to the html file to open.
-
-const args = process.argv.slice(2);
-const filePath = args[0];
-
+// Imports
 const puppeteer = require("puppeteer");
 const resolve = require("path").resolve;
 
+// NOTE:
+// The first custom argument should be the path to the GitHub repo's root.
+const args = process.argv.slice(2);
+const repoPath = args[0];
+const absoluteRepoPath = resolve(repoPath);
+
+// Main
 (async () => {
+  // Puppeteer setup
   const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox"]});
   const page = await browser.newPage({ waitUntil: "networkidle2" });
 
-  const absoluteFilePath = resolve(filePath);
-  console.log(`Absolute path: ${absoluteFilePath}`);
-  await page.goto(`file://${filePath}`);
-  await page.screenshot({ path: "./images/screenshot.png" });
+  // Screenshot
+  console.log(`Absolute repo path: ${absoluteRepoPath}`);
+  await page.goto(`file://${absoluteRepoPath}/index.html`);
+  await page.screenshot({ path: `${absoluteRepoPath}/images/screenshot.png` });
 
-  console.log("Captured screenshot");
   await browser.close();
 })().then((result) => {
-  console.log("Success:", result);
+  console.log("Screenshot succeeded:", result);
   process.exit(0);
 }).catch((error) => {
-  console.error("Error:", error);
+  console.error("Screenshot failed:", error);
   process.exit(1);
 });
