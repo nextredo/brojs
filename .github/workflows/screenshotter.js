33efd53704c531fb47e429226bef0eebd2d0f10b
nextredo <kb7737@gmail.com>
2026-06-04 15:30:23 +1000

    fix(cicd): fix cicd pt.6??????

--- .github/workflows/screenshotter.js
+++ .github/workflows/screenshotter.js
@@ -1,10 +1,11 @@
 const puppeteer = require("puppeteer");
+const resolve = require("path").resolve;
 
 (async () => {
   const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox"]});
   const page = await browser.newPage({ waitUntil: 'networkidle2' });
 
-  const filePath = path.resolve("./index.html");
+  const filePath = resolve("./index.html");
   await page.goto(`file://${filePath}`);
   await page.screenshot({ path: "./images/screenshot.png" });
 
